9b7214e6dce81203a7024d5abfca7e22cb022f0b
Gab Belmonte <belmonte.gabriel.k@gmail.com>
2017-12-03 16:45:24 -0500

    Layout improvements

--- Scripts/meditations.js
+++ Scripts/meditations.js
@@ -9,24 +9,38 @@ function AllChildren(element, type, depth)
 
 function CreateHierarchyInternal(node, childSelectors, depth)
 {
-    let children = AllChildren(node.val, childSelectors[depth], 0);
+    let children = AllChildren(node.val, childSelectors[depth], depth);
+    let leafsDepth = depth;
     for (let i = 0; i < children.length; ++i)
     {
         let child = children[i];
         let jChild = $(child);
-        let content = jChild.next();
         let childNode = {
             "text": jChild.text(),
+            "header": jChild[0].outerHTML,
+            "jVal": jChild,
             "val": child,
-            "width": content.width(),
-            "height": content.height(),
+            "parent": node,
             "children": []
         };
         childNode.toString = function () { return childNode.text; }
 
         node.children.push(childNode);
         CreateHierarchyInternal(childNode, childSelectors, depth + 1);
+
+        // Removing all children will leave nothing but the actual content 
+        // of the current node
+        jChild.next().remove();
+        jChild.remove();
+        //calculate final depth;
+        if (childNode.leafsDepth > leafsDepth)
+            leafsDepth = childNode.leafsDepth;
     }
+
+    if(depth !== 0)
+        node.content = node.jVal.next()[0].outerHTML;
+    node.depth = depth;
+    node.leafsDepth = leafsDepth;
 }
 
 function CreateHierarchy(childSelectors, root)
@@ -35,41 +49,54 @@ function CreateHierarchy(childSelectors, root)
         root = document;
 
     let rootNode = {
+        "text": "I AM ROOT",
         "val": root,
+        "jVal": $(root),
         "children": []
     };
     CreateHierarchyInternal(rootNode, childSelectors, 0);
     return rootNode;
 }
 
+let g_Radiuses = [[], [], [1500, 3000, 4500], [750, 2000], [750]];
+let g_Debug = true;
+
+function CreateSlides(node, center, angleDegParent, depth)
+{
+    let chilren = node.children;
+    for (let i = 0; i < chilren.length; ++i)
+    {
+        let child = chilren[i];
+
+        let angleDeg = (i / chilren.length) * 360 + angleDegParent;
+        let angleRad = angleDeg * Math.PI / 180;
+        let radius = g_Radiuses[child.depth][child.leafsDepth - child.depth];
+        let x = center.x + radius * Math.cos(angleRad);
+        let y = center.y + radius * Math.sin(angleRad);
+
+        let childHtml = g_Debug ? `<span>Radius ${radius} from parent '${child.parent.text}'</span>` : ""
+        childHtml += child.header + child.content;
+
+        $("#impress").append(`<div class='step slide' data-x='${x}' data-y='${y}' data-rotate='${angleDeg}' >${childHtml}</div>`);
+        CreateSlides(child, { "x": x, "y": y }, angleDeg, depth + 1);
+    }
+}
+
 function Load()
 {
-    let documentHierarchy = CreateHierarchy(["h1", "h2", "h3"], $("#byte-content")[0])
+    let documentHierarchy = CreateHierarchy([".med-title", "h1", "h2", "h3"], $("#byte-content")[0])
         .children;
     Meditations.Hierarchy = documentHierarchy;
     let number = documentHierarchy.length;
 
-    let radius = 1400;
-    let child = documentHierarchy[0];
+    let radius = 6000;
+    let root = documentHierarchy[0];
+    let html = root.header + root.content;
+    $("#impress").append(`<div class='step slide' data-x='0' data-y='0'>${html}</div>`);
+    CreateSlides(root, { "x": 0, "y": 0 }, 0 , 0);
 
-    jChild = $(child.val)
-    let childHtml = jChild[0].outerHTML + (jChild.next().html() != undefined ? jChild.next().html() : "")
-    $("#impress").append("<div class='step slide' data-x='0' data-y='0'>" + childHtml + "</div>")
-    for (let i = 1; i < number; ++i)
-    {
-        let child = documentHierarchy[i];
-        jChild = $(child.val)
-        childHtml = jChild[0].outerHTML + (jChild.next().html() != undefined ? jChild.next().html() : "")
-
-        let angleDeg = ((i - 1) / (number - 1)) * 360;
-        let angleRad = ((i - 1) / (number - 1)) * 2 * Math.PI;
-        let x = radius * Math.cos(angleRad);
-        let y = radius * Math.sin(angleRad);
-
-        $("#impress").append(`<div class='step slide' data-x='${x}' data-y='${y}' data-rotate='${angleDeg}' >${childHtml}</div>`);
-    }
-
-    $("#impress").append(`<div id="overview" class="step" data-x="0" data-y="0" data-z="0" data-scale="5"></div>`);
+    $("#impress").append(`<div id="overview" class="step" data-x="0" data-y="0" data-z="0" data-scale="15"></div>`);
+    $("#impress a").attr("target", "_new");
 }
 
 $(document).ready(
